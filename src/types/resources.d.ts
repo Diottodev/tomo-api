a704e140575761226a1a8201df71256f2edad0b7
DiottoDev <nicodiottodev@gmail.com>
2025-06-21 13:12:10 -0300

    fix: remove swagger d.ts

--- src/types/resources.d.ts
+++ src/types/resources.d.ts
@@ -5,26 +5,3 @@ declare module '@fastify/jwt' {
     user: { sub: string };
   }
 }
-declare module 'swagger-ui-express' {
-  import { RequestHandler } from 'express';
-  const swaggerUi: {
-    serve: RequestHandler[];
-    setup: (
-      spec: object,
-      options?: Record<string, unknown>,
-      customCss?: string,
-      customfavIcon?: string,
-      swaggerUrl?: string,
-      customSiteTitle?: string
-    ) => RequestHandler;
-  };
-  export = swaggerUi;
-}
-declare module 'swagger-jsdoc' {
-  interface SwaggerJSDocOptions {
-    definition: object;
-    apis: string[];
-  }
-  function swaggerJsdoc(options: SwaggerJSDocOptions): object;
-  export = swaggerJsdoc;
-}

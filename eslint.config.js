b10bf6e8d04552e4359de482a77ce343c1f187eb
DiottoDev <nicodiottodev@gmail.com>
2025-06-21 15:47:23 -0300

    chore: add eslint flat config for future migration
    - Add eslint.config.js for future ESLint v9+ migration
    - Currently using legacy .eslintrc.js for compatibility
    - Maintains current functionality while preparing for updates

---
+++ eslint.config.js
@@ -0,0 +1,20 @@
+﻿module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    '@typescript-eslint/recommended',
+    'prettier'
+  ],
+  plugins: ['@typescript-eslint', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    '@typescript-eslint/no-unused-vars': 'error',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn'
+  },
+  ignorePatterns: ['node_modules/', 'dist/', 'coverage/'],
+  env: {
+    node: true,
+    jest: true,
+    es6: true
+  }
+};
